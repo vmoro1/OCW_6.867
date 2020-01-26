e7b1a8cc36b00a8f5c63fb95c1233ef9eb222cc3
vmoro1 <42152733+vmoro1@users.noreply.github.com>
2020-01-26 18:30:23 +0100

    Account transfer

---
+++ UNIT_0/exercise_0.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 18:21:43 2019
+
+@author: viggomoro
+"""
+import numpy as np
+
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    return np.random.rand(n, 1)
+
+
+def operations(h, w):
+    """
+    Takes two inputs, h and w, and makes two Numpy arrays A and B of size
+    h x w, and returns A, B, and s, the sum of A and B.
+
+    Arg:
+      h - an integer describing the height of A and B
+      w - an integer describing the width of A and B
+    Returns (in this order):
+      A - a randomly-generated h x w Numpy array.
+      B - a randomly-generated h x w Numpy array.
+      s - the sum of A and B.
+    """
+    A = np.random.random([h, w])
+    B = np.random.random([h, w])
+    return (A, B, A + B)
+
+
+def norm(A, B):
+    """
+    Takes two Numpy column arrays, A and B, and returns the L2 norm of their
+    sum.
+
+    Arg:
+      A - a Numpy array
+      B - a Numpy array
+    Returns:
+      s - the L2 norm of A+B.
+    """
+    return np.linalg.norm(A + B)
+    
+
+def neural_network(inputs, weights):
+    """
+     Takes an input vector and runs it through a 1-layer neural network
+     with a given weight matrix and returns the output.
+
+     Arg:
+       inputs - 2 x 1 NumPy array
+       weights - 2 x 1 NumPy array
+     Returns (in this order):
+       out - a 1 x 1 NumPy array, representing the output of the neural network
+    """
+    weights_transposed = np.linalg.transpose(weights)
+    A = np.linalg.matmul(weights_transposed, inputs)
+    return np.tanh(A)
